3f2d3e7c7b1854f4bc58f589143be5b76ae9b1e4
clicmaster4285 <software.clickmasters@gmail.com>
2026-06-04 09:44:10 +0500

    add porigin

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,12 @@ export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
+  vite: {
+    preview: {
+      allowedHosts: [
+        "clickmasterssoftwaredevelopmentcompany.com",
+        "www.clickmasterssoftwaredevelopmentcompany.com",
+      ],
+    },
+  },
 });
