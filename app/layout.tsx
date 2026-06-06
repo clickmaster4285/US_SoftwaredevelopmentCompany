a6de0a9020b5f36e26605e83be7b2399e4c24adb
clicmaster4285 <software.clickmasters@gmail.com>
2026-06-06 10:31:10 +0500

    favico

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,17 @@ import './globals.css';
 import Providers from './providers';
 
 export const metadata = {
-   title: 'hmmmmmm....',
-   description: 'Immersive Hero creates an engaging hero section with a video that animates on scroll.',
+   title: 'ClickMasters',
+   description: 'ClickMasters is a leading provider of web development and digital marketing services.',
    openGraph: {
-      title: 'hmmmmmm....',
-      description: 'Immersive Hero creates an engaging hero section with a video that animates on scroll.',
+      title: 'ClickMasters',
+      description: 'ClickMasters is a leading provider of web development and digital marketing services.',
       type: 'website',
    },
    twitter: {
       card: 'summary',
-      title: 'hmmmmmm....',
-      description: 'Immersive Hero creates an engaging hero section with a video that animates on scroll.',
+      title: 'ClickMasters',
+      description: 'ClickMasters is a leading provider of web development and digital marketing services.',
    },
 };
 
